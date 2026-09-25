8dc6a38599ba6473d017357173a881f9000bd12c
[jarmo <toivanen@protonmail.com>
2026-09-25 18:25:34 +0300

    Add modbus-tcp gateway register
    gateway register gets basic conf: ip and port, ip will be filled via .env. Slave is binded to the correct device.. and address field was fixed.
    
    Added .gitignore exclusion for env*.
    
    modbus-tcp file uses readFile from node process to read the file string properly, but env still needs to have ./src/drivers/waveshare-gateway.json as env variable.

--- src/drivers/modbus-tcp.ts
+++ src/drivers/modbus-tcp.ts
@@ -1,43 +1,49 @@
 import modbuslib from 'modbus-serial'
+import { readFile } from 'node:fs/promises'
+import { resolve } from 'node:path'
 
 const Modbus = modbuslib.default
 const client = new Modbus()
 
-// add ZOD, import register JSON later
-
-const holdingRegisters = {
-  address: 0,
-  type: 'int32',
-  variable: 'V',
-  weight: 10,
-  length: 29,
+const gatewayConfigPath = process.env.WAVESHARE_GATEWAY_PATH
+if (!gatewayConfigPath) {
+  throw Error(`Missing env.var ${gatewayConfigPath}`)
 }
 
-const gatewayConfig = {
-  ip: 'XXXXX',
-  options: {
-    port: 502,
-    // // // // localAddress?: string,
-    // // // timeout: number,
-    // // socket?: Socket;
-    //     socketOpts?: SocketConstructorOpts;
-  },
+const json = await readFile(gatewayConfigPath, {
+  encoding: 'utf8',
+})
+// console.log(resolve(gatewayConfigPath))
+// console.log(typeof gatewayConfigPath) //STRING
+
+// console.log(json)
+// // console.log(typeof json)
+console.log(json)
+
+/**
+try {s
+  const filePath = new URL('./package.json', import.meta.url);
+  const contents = await readFile(filePath, { encoding: 'utf8' });
+  console.log(contents);
+} catch (err) {
+  console.error(err.message);
 }
+ */
 
 const meterConfig = {
   id: 1,
   responseTimeoutMs: 500,
 }
 
-await client.connectTcpRTUBuffered(gatewayConfig.ip, gatewayConfig.options)
-client.setID(meterConfig.id)
+// await client.connectTCP(gatewayConfig.ip, gatewayConfig.options)
+// client.setID(meterConfig.id)
 
-const { data, buffer } = await client.readHoldingRegisters(
-  holdingRegisters.address,
-  holdingRegisters.length,
-)
+// const { data, buffer } = await client.readHoldingRegisters(
+//   holdingRegisters.address,
+//   holdingRegisters.length,
+// )
 
-client.close()
+// client.close()
 
-console.log(data)
-console.log(buffer)
+// console.log(data)
+// console.log(buffer)
